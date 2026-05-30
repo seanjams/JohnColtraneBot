757335cdc9ad1e0b13e34988912c51fe88274b6e
Sean O'Reilly <seanvoreilly2@gmail.com>
2026-05-30 12:42:42 -0700

    ignore AskGrok

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ if not all((
         "No Reddit Credentials found in environment. Aborting."
     )
 
-IGNORE_USERNAMES = ["AliceColtraneBot"]
+IGNORE_USERNAMES = ["AliceColtraneBot", "AskGrok"]
 
 # set configuration values
 class Config:
